ee96d93ec1d885e123c862c90bca2490c7c86532
Bartłomiej Krzywania <b.krzywania@samsung.com>
2017-04-13 17:46:08 +0200

    Task list is now randomized
    Two task lists in the document to DEMONSTRATE,
    that they are completely independent instances

--- task-list/task-list.js
+++ task-list/task-list.js
@@ -1,45 +1,81 @@
-angular.module('taskList', [])
-   .directive('tasks', function() {
-      return {
-         restrict: 'E',
-         transclude: false,
-         // template: 'safxxxdsf',
-         templateUrl: 'task-list/task-list.html',
-         scope: {},
+(function () {
+   "use strict";
 
-         controller: function($scope, $element) {
+   function randomInt(from, to) {
+      return Math.floor(Math.random() * (1 + to - from)) + from;
+   }
 
-            $scope.elements = [
-               {label: 'posprzątać pokój', done: true},
-               {label: 'odnowić samochód', done: false}
-            ];
-            $scope.addNew = function() {
-               if(this.newLabel && this.newLabel.length) {
-                  this.elements.push(
-                     { label: this.newLabel, done: this.newDone }
-                  );
-               }
-               this.newLabel = "";
-            };
-            $scope.remaining = function() {
-               var count = 0;
-               angular.forEach(this.elements, function(element) {
-                  if(!element.done) {
-                     count++;
-                  }
-               });
-               return count;
-            }
-            $scope.archive = function() {
-               var oldElements = $scope.elements;
-               $scope.elements = [];
-               angular.forEach(oldElements, function(element) {
-                  if(!element.done) {
-                     $scope.elements.push(element);
-                  }
-               });
-            }
-            $scope.newDone = false;
+   let exampleTaskLabels = [
+      'do post office',
+      'sell car',
+      'call mum',
+      'tidy the desk',
+      'buy headphones',
+      'do the finances'
+   ];
+
+   function drawTasks() {
+      let taskCount = randomInt(1, exampleTaskLabels.length);
+      let tasks = [];
+
+      let labelsTaken = new Set();
+      for (let t = 0; t < taskCount; t += 1) {
+
+         let labelNo = randomInt(0, exampleTaskLabels.length - 1);
+         while (labelsTaken.has(labelNo)) {
+            labelNo = (labelNo + 1) % exampleTaskLabels.length;
          }
-      };
-   });
+         labelsTaken.add(labelNo);
+
+         tasks.push({
+            label: exampleTaskLabels[labelNo],
+            done: randomInt(1, 10) > 6
+         });
+      }
+
+      return tasks;
+   }
+
+   angular.module('taskList', [])
+      .directive('taskList', function () {
+         return {
+            restrict: 'E',
+            transclude: false,
+            templateUrl: 'task-list/task-list.html',
+            replace: true,
+            scope: {},
+
+            controller: function ($scope, $element) {
+               $scope.tasks = drawTasks();
+               $scope.addNew = function () {
+                  if ($scope.newLabel && $scope.newLabel.length) {
+                     $scope.tasks.push({
+                        label: $scope.newLabel,
+                        done: $scope.newDone
+                     });
+                  }
+                  $scope.newLabel = "";
+               };
+               $scope.remaining = function () {
+                  let count = 0;
+                  angular.forEach($scope.tasks, function (task) {
+                     if (!task.done) {
+                        count += 1;
+                     }
+                  });
+                  return count;
+               };
+               $scope.archive = function () {
+                  let oldTasks = $scope.tasks;
+                  $scope.tasks = [];
+                  angular.forEach(oldTasks, function (task) {
+                     if (!task.done) {
+                        $scope.tasks.push(task);
+                     }
+                  });
+               };
+               $scope.newDone = false;
+            }
+         };
+      });
+}());
